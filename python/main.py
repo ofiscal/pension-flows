7b528acbec178ad0af67ed0a28830a70c1a4a68d
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-01-26 16:19:32 -0500

    load data faster and more beautifully

--- python/main.py
+++ python/main.py
@@ -1,60 +1,37 @@
+from typing import List, Dict
 from os.path import join
 import pandas as pd
 
 
-datapath = "data/geih/2021-11/csv/"
+datapath : str = "data/geih/2021-11/csv"
 
+columns_of_interest_original_names : List [str] = \
+    [ "fex_c_2011", "DIRECTORIO", "ORDEN",
+      "INGLABO", "P6920", "P6430" ]
 
-### ### ### ### ### ### ###
-###    Read the data    ###
-### ### ### ### ### ### ###
+rename_columns : Dict [ str, str ] = \
+  { "fex_c_2011" : "weight",
+    "INGLABO"    : "labor income",
+    "P6920"      : "pension contrib",
+    "P6430"      : "independiente" }
 
-# TODO : This is inefficient --
-# it reads every column, most of which are unneeded.
+area = (
+  pd.read_csv (
+    join ( datapath, "area_Ocupados.csv" ),
+    usecols = columns_of_interest_original_names,
+    sep = ";" )
+  . rename ( columns = rename_columns ) )
 
-area = pd.read_csv(
-  join ( datapath, "area_Ocupados.csv" ),
-  sep = ";" )
+cabecera = (
+  pd.read_csv(
+    join ( datapath, "Cabecera_Ocupados.csv" ),
+    usecols = columns_of_interest_original_names,
+    sep = ";" )
+  . rename ( columns = rename_columns ) )
 
-cabecera = pd.read_csv(
-  join ( datapath, "Cabecera_Ocupados.csv" ),
-  sep = ";" )
-
-resto = pd.read_csv(
-  join ( datapath, "Resto_Ocupados.csv" ),
-  sep = ";" )
-
-
-### ### ### ### ### ### ### ###
-###     Subset the data     ###
-### ### ### ### ### ### ### ###
-
-area2 = (
-  area [[ "fex_c_2011", "DIRECTORIO", "ORDEN",
-          "INGLABO", "P6920", "P6430" ]]
-  . copy()
-  . rename (
-    columns = { "fex_c_2011" : "weight",
-                "INGLABO"    : "labor income",
-                "P6920"      : "pension contrib",
-                "P6430"      : "independiente" } ) )
-
-cabecera2 = (
-  cabecera [[ "fex_c_2011", "DIRECTORIO", "ORDEN",
-              "INGLABO", "P6920", "P6430" ]]
-  . copy()
-  . rename (
-    columns = { "fex_c_2011" : "weight",
-                "INGLABO"    : "labor income",
-                "P6920"      : "pension contrib",
-                "P6430"      : "independiente" } ) )
-
-resto2 = (
-  resto [[ "fex_c_2011", "DIRECTORIO", "ORDEN",
-           "INGLABO", "P6920", "P6430" ]]
-  . copy()
-  . rename (
-    columns = { "fex_c_2011" : "weight",
-                "INGLABO"    : "labor income",
-                "P6920"      : "pension contrib",
-                "P6430"      : "independiente" } ) )
+resto = (
+  pd.read_csv(
+    join ( datapath, "Resto_Ocupados.csv" ),
+    usecols = columns_of_interest_original_names,
+    sep = ";" )
+  . rename ( columns = rename_columns ) )
