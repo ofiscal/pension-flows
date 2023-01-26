906f184ebafb4f9faaa3ea7006f4ebc099e91d1d
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-01-26 12:01:21 -0500

    load, prettify data

--- code.py
+++ code.py
@@ -1,2 +1,59 @@
-ppl = pd.read_csv("data/mpmd/Personas/Personas.csv", sep=";")
-hhs = pd.read_csv("data/mpmd/Hogares/Hogares.csv", sep=";")
+from os.path import join
+import pandas as pd
+
+
+datapath = "data/geih/2021-11/csv/"
+
+
+### ### ### ### ### ###
+### read the data   ###
+### ### ### ### ### ###
+
+## TODO : This is inefficient -- it reads everything, most of which is unneeded."
+
+area = pd.read_csv(
+  join ( datapath, "area_Ocupados.csv" ),
+  sep = ";" )
+
+cabecera = pd.read_csv(
+  join ( datapath, "Cabecera_Ocupados.csv" ),
+  sep = ";" )
+
+resto = pd.read_csv(
+  join ( datapath, "Resto_Ocupados.csv" ),
+  sep = ";" )
+
+
+### ### ### ### ### ###
+### subset the data ###
+### ### ### ### ### ###
+
+area2 = (
+  area [[ "fex_c_2011", "DIRECTORIO", "ORDEN",
+          "INGLABO", "P6920", "P6430" ]]
+  . copy()
+  . rename (
+    columns = { "fex_c_2011" : "weight",
+                "INGLABO"    : "labor income",
+                "P6920"      : "pension contrib",
+                "P6430"      : "independiente" } ) )
+
+cabecera2 = (
+  cabecera [[ "fex_c_2011", "DIRECTORIO", "ORDEN",
+              "INGLABO", "P6920", "P6430" ]]
+  . copy()
+  . rename (
+    columns = { "fex_c_2011" : "weight",
+                "INGLABO"    : "labor income",
+                "P6920"      : "pension contrib",
+                "P6430"      : "independiente" } ) )
+
+resto2 = (
+  resto [[ "fex_c_2011", "DIRECTORIO", "ORDEN",
+           "INGLABO", "P6920", "P6430" ]]
+  . copy()
+  . rename (
+    columns = { "fex_c_2011" : "weight",
+                "INGLABO"    : "labor income",
+                "P6920"      : "pension contrib",
+                "P6430"      : "independiente" } ) )
