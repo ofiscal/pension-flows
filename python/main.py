cce8bf898f4053179c3836948cc0005490988c86
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-01-26 21:06:48 -0500

    compute pension contributions in main.py

--- python/main.py
+++ python/main.py
@@ -4,8 +4,19 @@ import pandas as pd
 from python.get_data import ( deduplicate_rows,
                               interpret_columns,
                               raw_renamed_data )
+from python.ss_functions import ( mk_pension,
+                                  mk_pension_employer )
 
 
 ppl = interpret_columns (
   deduplicate_rows (
     raw_renamed_data () ) )
+
+for (new_col, function) in [
+    ("employee contribs", mk_pension         ),
+    ("employer contribs", mk_pension_employer) ]:
+  ppl[new_col] = ppl.apply (
+    lambda row: function (
+      row["independiente"],
+      row["labor income"] )
+    , axis = "columns" )
