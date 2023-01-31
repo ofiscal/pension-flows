cdf4a1e01ace730d94f2b7de2658300c0eb99305
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-01-31 10:39:39 -0500

    ingest gender ("female" : bool)

--- python/get_data.py
+++ python/get_data.py
@@ -15,7 +15,7 @@ columns_of_interest_original_names_ocupados : \
 columns_of_interest_original_names_caracteristicas_personales : \
   List [str] = (
     columns_of_interest_original_names_universal +
-    [ "P6030S3" ] )
+    [ "P6020", "P6030S3" ] )
 
 rename_columns_universal : \
   Dict [ str, str ] = \
@@ -44,6 +44,7 @@ rename_columns_caracteristicas_personales : \
       # are names of what the column *will be*
       # once processed by some interpret_columns_ function.
       # Before that, the names on the RHS below are a lie.
+      "P6020"   : "female",
       "P6030S3" : "age" }
 
 def fetch_one ( filename : str,
@@ -98,6 +99,8 @@ def interpret_columns_caracteristicas_personales (
     df ["age"]
     . str.replace ( ",", "" )
     . astype ('float') )
+  df["female"] = 2021 - (
+    df ["female"] - 1 )
   return interpret_columns_universal ( df )
 
 def interpret_columns_ocupados ( df : pd.DataFrame
--- python/main.py
+++ python/main.py
@@ -10,6 +10,7 @@ from python.get_data import ( deduplicate_rows,
 from python.ss_functions import ( mk_pension,
                                   mk_pension_employer )
 
+
 ocup = interpret_columns_ocupados (
   deduplicate_rows (
     raw_ocupados_renamed () ) )
@@ -19,11 +20,22 @@ cg = interpret_columns_caracteristicas_personales (
   deduplicate_rows (
     raw_caracteristicas_generales_renamed () ) )
 
+# Not everyone is in Ocupados.
+# The default "both" merge strategy returns the intersection,
+# and hence only the Ocupados,
+# whereas a left merge should give everyone
+# (assuming  Caracteristicas_Personales describes everyone).
+allData = cg.merge ( ocup,
+                     how = "left",
+                     on = ["DIR","SEC","ORD"] )
+
+allOcup = cg.merge ( ocup,
+                     on = ["DIR","SEC","ORD"] )
 
 for (new_col, function) in [
     ("employee contribs", mk_pension         ),
     ("employer contribs", mk_pension_employer) ]:
-  ppl[new_col] = ppl.apply (
+  ocup[new_col] = ocup.apply (
     lambda row: function (
       row["independiente"],
       row["labor income"] )
