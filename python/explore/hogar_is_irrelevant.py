a09def1a35f6720a2203cad679f8bd6d34f9b606
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-02-01 17:03:05 -0500

    Add code to show that HOGAR is redundant,
      equal everywhere to SECUENCIA_P.

---
+++ python/explore/hogar_is_irrelevant.py
@@ -0,0 +1,28 @@
+# PURPOSE:
+# Show that HOGAR is redundant, everywhere equal to SECUENCIA_P.
+
+# USAGE | PITFALL:
+# This requires modifying get_data.py,
+# by inserting the line
+#   "HOGAR" : "HOG",
+# into the definition of `dicts_to_rename_columns["universal"]`.
+# This makes the (redundant) HOGAR variable available.
+
+cg = interpret_columns_caracteristicas_personales (
+  deduplicate_rows (
+    raw_caracteristicas_generales_renamed () ) )
+otros = interpret_columns_otros_ingresos (
+  deduplicate_rows (
+    raw_otros_ingresos_renamed () ) )
+ocup = mk_pension_contribs (
+  interpret_columns_ocupados (
+    deduplicate_rows (
+      raw_ocupados_renamed () ) ) )
+
+pkh = primary_keys + ["HOG"]
+df = pd.concat ( [ cg[pkh],
+                   ocup[pkh],
+                   otros[pkh], ] )
+s = df["SEC"] == df["HOG"]
+s.describe()
+s.isnull().sum()
