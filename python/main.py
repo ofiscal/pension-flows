8031f82b12c3db3da2b6b25ec78181a429aa0819
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-02-06 20:13:03 -0500

    draw CDF of pension income

--- python/main.py
+++ python/main.py
@@ -1,7 +1,18 @@
 from typing import List, Dict, Tuple
 import pandas as pd
+import matplotlib.pyplot as plt
 
 from python.get_data import mkData
+from ofiscal_utils.draw.draw import cdf
 
 
 df = mkData()
+
+if True: # draw pension income
+  cdf( df["pension income"],
+       logx = False )
+  plt.title("CDF of pension icnome")
+  plt.xlabel("Income")
+  plt.ylabel("Probability")
+  plt.savefig("test-cdf.png")
+  plt.close()
