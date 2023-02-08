d4cf89dd7e10e75eda44d72cd163e1ccd11eaea8
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-02-07 19:05:05 -0500

    toward computing the CDF of every possible subset
      selecting on gender, formality and area (rural/urban)

--- python/main.py
+++ python/main.py
@@ -8,11 +8,51 @@ from ofiscal_utils.draw.draw import cdf
 
 df = mkData()
 
-if True: # draw pension income
-  cdf( df["pension income"],
-       logx = False )
-  plt.title("CDF of pension icnome")
-  plt.xlabel("Income")
-  plt.ylabel("Probability")
-  plt.savefig("test-cdf.png")
+def draw_cdf_of (
+    colname : str,
+    df : pd.DataFrame,
+    subsample : str
+): # pure IO [writes a file]
+  cdf( df[colname],
+       logx = True )
+  plt.title("CDF of " + colname)
+  plt.xlabel("COP")
+  plt.ylabel("Probability mass")
+  plt.savefig( "CDF-of-" + colname   +
+               "-for-"   + subsample +
+               ".png" )
   plt.close()
+
+def df_subset ( female   : int = None,
+                area     : str = None,
+                informal : int = None
+               ) -> pd.DataFrame:
+
+  def subsetter ( df : pd.DataFrame,
+                  colname : str,
+                  colval # Its type depends on colname's type:
+                         # int if colname in [female, informal]
+                         # str if colname == area
+                 ) -> pd.DataFrame:
+    return ( df
+             if colval is None
+             else df[ df["colname"] == colval] )
+
+  return subsetter (
+    subsetter (
+      subsetter ( df, "female", female ),
+      "area", area ),
+    "informal", informal )
+
+
+
+[ (a,b)
+  for a in [0,1,None]
+  for b in [0,1] ]
+
+women    = df[ df["female"]        == 1]
+men      = df[ df["female"]        == 0]
+rural    = df[ df["source file"]   == "area"]
+urban    = df[ df["source file"]   == "cabecera"]
+informal = df[ df['independiente"] == 1]
+formal   = df[ df["independiente"] == 0]
