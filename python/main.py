8e41da28d379f06a43d09d5a517bfc963e79e11e
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-02-09 09:56:21 -0500

    Write "no data satisfies _ constraints" warnings to a file.
    Reduce a function's responsibilities
    
      by adding to its argument list:
    
        def draw_cdf_of (
            colname         : str,
            df              : pd.DataFrame,
            ct              : Dict[ str, Any ],
       +    output_filename : str,
    
    Redirect output to a subfolder.

--- python/main.py
+++ python/main.py
@@ -1,3 +1,4 @@
+from os.path import join
 from typing import List, Dict, Tuple, Any
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -11,9 +12,10 @@ from python.lib import ( constraint_string,
 df = mkData()
 
 def draw_cdf_of (
-    colname : str,
-    df : pd.DataFrame,
-    ct : Dict[ str, Any ],
+    colname         : str,
+    df              : pd.DataFrame,
+    ct              : Dict[ str, Any ],
+    output_filename : str,
 ): # pure IO [writes a file]
   cdf( df[colname],
        logx = True,
@@ -21,9 +23,7 @@ def draw_cdf_of (
   plt.title("CDF of " + colname)
   plt.xlabel("COP")
   plt.ylabel("Probability mass")
-  plt.savefig( "CDF-of-" + colname
-               + constraint_string( ct )
-               + ".png" )
+  plt.savefig( output_filename )
   plt.close()
 
 constraints : List[ Dict[ str, Any ] ] = [
@@ -37,14 +37,26 @@ constraints : List[ Dict[ str, Any ] ] = [
   for formal in [0,1]
   for indep  in [0,1] ]
 
+output_folder = "output/cdfs/"
+missing_cdfs_path = join ( output_folder, "MISSING-CDFS.txt" )
+
+with open( missing_cdfs_path, "w" ) as myfile:
+  myfile.write( "" ) # Empty the file.
+
 for ct in constraints:
   for col in [ "labor income", "pension income" ]:
-    if len(df_subset) == 0:
-      print( "WARNING: No data satisfies " + str(ct) )
     df_subset = ( df_subset_from_constraints ( df, ct )
                   . dropna() )
+    if len(df_subset) == 0:
+      with open(missing_cdfs_path, "w") as missing_cdfs_file:
+        missing_cdfs_file.write ( "WARNING: No data satisfies "
+                                  + str(ct) )
     else:
       draw_cdf_of (
         colname = col,
         df = df_subset,
-        ct = ct )
+        ct = ct,
+        output_filename = join (
+          output_folder, ( "CDF-of-" + col
+                           + constraint_string( ct )
+                           + ".png" ) ) )
