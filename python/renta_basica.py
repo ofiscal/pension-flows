d8c63210f4abb4bf30ab0c2ba15b5880a8869259
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-02-09 20:47:49 -0500

    nothing
      comments
      layout

--- python/renta_basica.py
+++ python/renta_basica.py
@@ -12,6 +12,8 @@ from python.lib import ( constraint_string,
 
 def near_nonzero (a : float, b : float) -> bool:
   # TODO: This should be part of ofiscal_utils.
+  # tax.co has a similar, better function.
+  """Verifies that its two inputs are within 1% of each other."""
   return ( False
            if a < (0.99*b)
            else ( False if b < (0.99*a)
@@ -24,11 +26,15 @@ def test_near_nonzero ():
   assert near_nonzero(1,1.00001) == True
   assert near_nonzero(1.00001,1) == True
 
-subsidy_if_broke = 500e3 # COP per month
-when_subsidy_starts_to_wane = 2e6 # COP per month
-when_subsidy_disappears = 4e6 # COP per month
-negative_dSubsidy_dIncome = ( # PITFALL: The *negative* of the derivative.
-  # I think that makes the definition of "subsidy" easier to understand.
+subsidy_if_broke            = 500e3 # COP per month
+when_subsidy_starts_to_wane = 2e6   # COP per month
+when_subsidy_disappears     = 4e6   # COP per month.
+  # Should be bigger than `when_subsidy_starts_to_wane`.
+
+negative_dSubsidy_dIncome = ( # PITFALL:
+  # Since this is a positive number,
+  # it is the *negative* of the derivative dSubsidy / dIncome.
+  # I think that makes the definition of `subsidy()` easier to understand.
   subsidy_if_broke
   / (when_subsidy_disappears
      - when_subsidy_starts_to_wane) )
