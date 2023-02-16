a606d92eeba383dd1e551f82ad10dd54589a71cd
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-02-16 17:08:52 -0500

    python.build.nov2022.mkData() -> pd.DataFrame

--- python/build/nov2022.py
+++ python/build/nov2022.py
@@ -5,8 +5,13 @@ import pandas as pd
 
 from python.ss_functions import ( mk_pension,
                                   mk_pension_employer, )
-from python.build.common import ( primary_keys,
-                                  dicts_to_rename_columns )
+from python.build.common import (
+  primary_keys,
+  dicts_to_rename_columns,
+  interpret_columns_caracteristicas_personales,
+  interpret_columns_ocupados,
+  deduplicate_rows,
+  mk_pension_contribs )
 
 
 if True: # Add 2022-specific variables to `dicts_to_rename_columns`.
@@ -51,3 +56,39 @@ def raw_otros_ingresos_renamed () -> pd.DataFrame:
     how_to_rename_columns = {
       ** dicts_to_rename_columns["universal"],
       ** dicts_to_rename_columns["otros_ingresos"] } )
+
+def mkData () -> pd.DataFrame:
+  cg = interpret_columns_caracteristicas_personales (
+    deduplicate_rows (
+      raw_caracteristicas_generales_renamed (),
+      primary_keys = primary_keys ) )
+  otros = deduplicate_rows ( # No interpretation needed.
+    raw_otros_ingresos_renamed (),
+    primary_keys = primary_keys )
+  ocup = mk_pension_contribs ( # This extra step is not present
+                               # in the other two tables.
+    interpret_columns_ocupados (
+      deduplicate_rows (
+        raw_ocupados_renamed (),
+        primary_keys = primary_keys ) ) )
+
+  m = pd.merge (
+    otros.drop ( columns = ["weight"] ),
+    pd.merge (
+      cg,
+      ocup.drop ( columns = ["weight"] ),
+      how = "outer",
+      on = primary_keys ),
+    how = "outer",
+    on = primary_keys )
+
+  m["in ocupados"] = m["in ocupados"] . fillna(0)
+
+  for c in ["pension income",
+            "labor income",
+            "rental income",
+            "employee contribs",
+            "employer contribs",]:
+    m[c] = m[c].fillna(0)
+
+  return m
