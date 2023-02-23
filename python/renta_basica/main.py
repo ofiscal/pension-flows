278a3a7986d004ad0215b47c0d193d75882417ab
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2023-02-23 13:07:46 -0500

    Change a default
      from all_reports() (which is expensive)
      to selected_reports().

--- python/renta_basica/main.py
+++ python/renta_basica/main.py
@@ -7,7 +7,7 @@ from python.renta_basica.lib import ( readScenarios,
 if False: # Keeps it from running every time I run pytest
   df = mkData()
   scenarios = readScenarios()
-  res = all_reports( # or selected_reports()
+  res = selected_reports( # or all_reports()
     df        = df,
     scenarios = scenarios )
-  res.to_excel ( "basic_income_simulations.all-144.xlsx" )
+  res.to_excel ( "basic_income_simulations.selected.xlsx" )
